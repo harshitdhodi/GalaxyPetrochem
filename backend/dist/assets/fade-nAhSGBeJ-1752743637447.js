de36ee24146872d198318845f97eead2577ee77a
harshitdhodi <harshit.dhodi2108@gmail.com>
2025-07-17 14:57:10 +0530

    update brand page title and bradcrumb text size

--- backend/dist/assets/fade-nAhSGBeJ-1752743637447.js
+++
@@ -1,4 +0,0 @@
-import{ae as r,y as i}from"./index-B9f-WdoL-1752743637447.js";import"./index-AN08iotH-1752743637447.js";import"./AntdIcon-z0HYzSve-1752743637447.js";const s=new i("antFadeIn",{"0%":{opacity:0},"100%":{opacity:1}}),m=new i("antFadeOut",{"0%":{opacity:1},"100%":{opacity:0}}),l=function(a){let e=arguments.length>1&&arguments[1]!==void 0?arguments[1]:!1;const{antCls:o}=a,n=`${o}-fade`,t=e?"&":"";return[r(n,s,m,a.motionDurationMid,e),{[`
-        ${t}${n}-enter,
-        ${t}${n}-appear
-      `]:{opacity:0,animationTimingFunction:"linear"},[`${t}${n}-leave`]:{animationTimingFunction:"linear"}}]};export{l as i};
