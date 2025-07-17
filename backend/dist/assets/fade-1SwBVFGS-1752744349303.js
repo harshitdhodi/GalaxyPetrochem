1a290e7011f3e1ba2e939901c013e2046fd1bf62
harshitdhodi <harshit.dhodi2108@gmail.com>
2025-07-17 15:01:21 +0530

    update thank you page path

--- backend/dist/assets/fade-1SwBVFGS-1752744349303.js
+++
@@ -1,4 +0,0 @@
-import{ae as r,y as i}from"./index-1MV207t0-1752744349303.js";import"./index-xzxvMSST-1752744349303.js";import"./AntdIcon-V9u8an_1-1752744349303.js";const s=new i("antFadeIn",{"0%":{opacity:0},"100%":{opacity:1}}),m=new i("antFadeOut",{"0%":{opacity:1},"100%":{opacity:0}}),l=function(a){let e=arguments.length>1&&arguments[1]!==void 0?arguments[1]:!1;const{antCls:o}=a,n=`${o}-fade`,t=e?"&":"";return[r(n,s,m,a.motionDurationMid,e),{[`
-        ${t}${n}-enter,
-        ${t}${n}-appear
-      `]:{opacity:0,animationTimingFunction:"linear"},[`${t}${n}-leave`]:{animationTimingFunction:"linear"}}]};export{l as i};
