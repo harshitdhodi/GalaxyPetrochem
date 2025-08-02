532ddd2fd3be4038c80da4f4a732eecb7e9a079c
harshitdhodi <harshit.dhodi2108@gmail.com>
2025-08-02 11:00:42 +0530

    update footer link

--- frontend/src/website/componets/home/footer/CorporateSection.jsx
+++ frontend/src/website/componets/home/footer/CorporateSection.jsx
@@ -6,22 +6,22 @@ export default function CorporateSection({ navigate }) {
       <h2 className="text-xl font-medium mb-6">Quick Links</h2>
       <ul className="grid grid-cols-2 gap-x-8 gap-y-3 text-sm">
         <li>
-          <ScrollLink to="/introduction" className="hover:text-gray-200 block" navigate={navigate}>
+          <ScrollLink to="/about-us" className="hover:text-gray-200 block" navigate={navigate}>
             About Us
           </ScrollLink>
         </li>
         <li>
-          <ScrollLink to="/introduction" className="hover:text-gray-200 block" navigate={navigate}>
+          <ScrollLink to="/industrial-oils" className="hover:text-gray-200 block" navigate={navigate}>
             Products
           </ScrollLink>
         </li>
         <li>
-          <ScrollLink to="/vision-mission" className="hover:text-gray-200 block" navigate={navigate}>
+          <ScrollLink to="/blogs" className="hover:text-gray-200 block" navigate={navigate}>
             Blogs
           </ScrollLink>
         </li>
         <li>
-          <ScrollLink to="/vision-mission" className="hover:text-gray-200 block" navigate={navigate}>
+          <ScrollLink to="/brands" className="hover:text-gray-200 block" navigate={navigate}>
             Brands
           </ScrollLink>
         </li>
